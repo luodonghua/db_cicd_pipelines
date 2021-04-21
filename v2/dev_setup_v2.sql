51a88a2884ee1da567d66f637c4dd6a857d4719e
Donghua <luodonghua@gmail.com>
2021-04-21 10:20:20 +0800

    v2 changes

---
+++ v2/dev_setup_v2.sql
@@ -0,0 +1,10 @@
+alter table t_salesrep add (commission number);
+create or replace procedure p_payroll
+as 
+	v_salary 	number;
+	v_commision	number;
+begin
+	-- omit implementation detail 
+	null;
+end;
+/
